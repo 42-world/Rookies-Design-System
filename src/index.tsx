9d055a2a8fb137e07b8975932de2e6d3ca326fd9
huni <petercha2000@gmail.com>
2022-10-15 17:30:25 +0900

    feat: indexx export 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
-export { Button } from "./Button";
+export * from "./atoms";
 export { Battery } from "./Battery";
+export { Button } from "./Button";
 export { MotionButton } from "./MotionButton";
