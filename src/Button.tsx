f25d4b64da8683240af602243869ac2651a8ab5c
huni <petercha2000@gmail.com>
2022-10-15 17:42:02 +0900

    fix: 안쓰는거 삭제 및 이름 변경

--- src/Button.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import styles from "./Button.modules.css";
-
-export function Button({ title = "Title" }) {
-  return <button className={styles.button}>{title}</button>;
-}
--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from "./atoms";
-export { Battery } from "./Battery";
-export { Button } from "./Button";
-export { MotionButton } from "./MotionButton";
