40266770d3aef8ab4b516e71687a57a31db8bfe5
Son Beomsoo <k1a11220@naver.com>
2022-10-22 13:11:36 +0900

    chore: css 수정

--- src/Atoms/Chip.tsx
+++ src/Atoms/Chip.tsx
@@ -26,46 +26,28 @@ export function Chip({ text }: Props) {
 }
 
 const unSelectedStyle = css`
-  box-sizing: border-box;
-  width: min-content; /* 64px */
-  height: min-content; /* 35px */
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 6px 18px 6px 18px;
-  overflow: hidden;
   align-content: center;
-  flex-wrap: nowrap;
-  gap: 10;
   border-radius: 8px;
   border: 1px solid var(--grey_30_light, #e9ebee);
-  font-size: 16px;
+  background-color: #fff;
 `;
 
 const unSelectedTextStyle = css`
-  flex-shrink: 0;
-  width: auto; /* 28px */
-  height: auto; /* 23px */
-  white-space: pre;
-  position: relative;
-  line-height: 1.2;
+  font-size: 16px;
+  margin: 0;
 `;
 
 const selectedTextStyle = css`
-  flex-shrink: 0;
-  width: auto; /* 28px */
-  height: auto; /* 23px */
-  white-space: pre;
-  position: relative;
   color: var(--grey_5_light, #fefefe);
-  line-height: 1.2;
+  font-size: 16px;
+  margin: 0;
 `;
 
 const selectedStyle = css`
-  box-sizing: border-box;
-  width: min-content; /* 64px */
-  height: min-content; /* 35px */
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -74,7 +56,6 @@ const selectedStyle = css`
   background-color: var(--main_green_10, #41c464);
   overflow: hidden;
   align-content: center;
-  flex-wrap: nowrap;
   gap: 10;
   border-radius: 8px;
 `;
