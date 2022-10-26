b060ebc647dc9810fea835d09b4f7d6abce60783
Son Beomsoo <k1a11220@naver.com>
2022-10-26 21:43:38 +0900

    feat: 버튼 컴포넌트 구현

---
+++ src/components/Button.tsx
@@ -0,0 +1,59 @@
+import { css } from "@emotion/css";
+import * as React from "react";
+import { tokens } from "../tokens";
+
+type Props = {
+  theme: "light" | "dark";
+  text: string;
+  link: string;
+  size: "normal" | "small";
+  type: "default" | "danger" | "primary";
+};
+
+export function Chip({ text, link, size, type }: Props) {
+  return (
+    <button
+      type="button"
+      className={css`
+        padding: ${size === "normal"
+          ? "8px 24px 8px 24px"
+          : "4px 24px 4px 24px"};
+        border-radius: 8px;
+        background-color: ${type === "primary"
+          ? tokens.color.main_green_10.value
+          : tokens.color.grey_10_light.value};
+        border: 1px solid ${tokens.color.grey_30_light.value};
+        color: ${type === "primary"
+          ? tokens.color.grey_5_light.value
+          : tokens.color.grey_50_light.value};
+        font-weight: 500;
+        font-size: 16px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        cursor: pointer;
+        white-space: nowrap;
+        vertical-align: baseline;
+        line-height: 1.875;
+
+        font-family: "Pretendard Variable", Pretendard, -apple-system,
+          BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
+          "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
+          "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+
+        @font-face {
+          font-family: "Pretendard Variable", Pretendard, -apple-system,
+            BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
+            "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
+            "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+          src: url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css");
+        }
+      `}
+    >
+      {text}
+    </button>
+  );
+}
+
+const sizeNormal = css``;
+const sizeSmall = css``;
