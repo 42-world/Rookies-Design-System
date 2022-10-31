5275e3dc55bb1efa1ba4d6150b0092e033b523db
Son Beomsoo <k1a11220@naver.com>
2022-10-31 21:28:39 +0900

    feat: 버튼 다크모드 구현

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,42 +10,106 @@ type Props = {
   type: 'default' | 'danger' | 'primary';
 };
 
-export function Button({ text, link, size, type }: Props) {
+export function Button({ theme, text, link, size, type }: Props) {
+  const defaultStyle = css`
+    padding: ${size === 'normal' ? '8px 24px 8px 24px' : '4px 24px 4px 24px'};
+    border-radius: 8px;
+    background-color: ${theme === 'light' ? tokens.color.grey_10_light : tokens.color.grey_10_dark};
+    border: 1px solid ${theme === 'light' ? tokens.color.grey_30_light : tokens.color.grey_30_dark};
+    color: ${theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark};
+    font-weight: 500;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
+    white-space: nowrap;
+    vertical-align: baseline;
+    line-height: 1.875;
+
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+
+    @font-face {
+      font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+        'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+      src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
+    }
+  `;
+
+  const dangerStyle = css`
+    padding: ${size === 'normal' ? '8px 24px 8px 24px' : '4px 24px 4px 24px'};
+    border-radius: 8px;
+    background-color: ${theme === 'light' ? tokens.color.grey_10_light : tokens.color.grey_10_dark};
+    border: 1px solid ${theme === 'light' ? tokens.color.grey_30_light : tokens.color.grey_30_dark};
+    color: ${theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark};
+    font-weight: 500;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
+    white-space: nowrap;
+    vertical-align: baseline;
+    line-height: 1.875;
+
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+
+    @font-face {
+      font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+        'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+      src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
+    }
+  `;
+
+  const primaryStyle = css`
+    padding: ${size === 'normal' ? '8px 24px 8px 24px' : '4px 24px 4px 24px'};
+    border-radius: 8px;
+    background-color: ${tokens.color.main_green_10};
+    border: 1px solid ${tokens.color.grey_30_light};
+    color: ${tokens.color.grey_5_light};
+    font-weight: 500;
+    font-size: 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
+    white-space: nowrap;
+    vertical-align: baseline;
+    line-height: 1.875;
+
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+
+    @font-face {
+      font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+        'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+      src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
+    }
+  `;
+
   return (
-    <button
+    <a
       type="button"
-      className={css`
-        padding: ${size === 'normal' ? '8px 24px 8px 24px' : '4px 24px 4px 24px'};
-        border-radius: 8px;
-        background-color: ${type === 'primary' ? tokens.color.main_green_10 : tokens.color.grey_10_light};
-        border: 1px solid ${tokens.color.grey_30_light};
-        color: ${type === 'primary' ? tokens.color.grey_5_light : tokens.color.grey_50_light};
-        font-weight: 500;
-        font-size: 16px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        cursor: pointer;
-        white-space: nowrap;
-        vertical-align: baseline;
-        line-height: 1.875;
-
-        font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
-          'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
-          'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
-
-        @font-face {
-          font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
-            'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
-            'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
-          src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
-        }
-      `}
+      className={
+        type === 'default'
+          ? defaultStyle
+          : type === 'danger'
+          ? dangerStyle
+          : type === 'primary'
+          ? primaryStyle
+          : defaultStyle
+      }
+      href={link}
     >
       {text}
-    </button>
+    </a>
   );
 }
-
-const sizeNormal = css``;
-const sizeSmall = css``;
