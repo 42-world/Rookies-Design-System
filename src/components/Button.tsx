f5a6e15736dd86ea4431d9d8cb390026e99c9e41
Son Beomsoo <k1a11220@naver.com>
2022-10-31 21:56:18 +0900

    feat: 버튼 hover 상태 구현

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -37,6 +37,10 @@ export function Button({ theme, text, link, size, type }: Props) {
         'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
       src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
     }
+
+    &:hover {
+      background-color: ${theme === 'light' ? tokens.color.grey_20_light : tokens.color.grey_20_dark};
+    }
   `;
 
   const dangerStyle = css`
@@ -65,13 +69,19 @@ export function Button({ theme, text, link, size, type }: Props) {
         'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
       src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
     }
+
+    &:hover {
+      background-color: ${theme === 'light' ? tokens.color.red_5_light : tokens.color.red_5_dark};
+      color: ${tokens.color.red_10_light};
+      border: 1px solid ${tokens.color.red_10_light};
+    }
   `;
 
   const primaryStyle = css`
     padding: ${size === 'normal' ? '8px 24px 8px 24px' : '4px 24px 4px 24px'};
     border-radius: 8px;
     background-color: ${tokens.color.main_green_10};
-    border: 1px solid ${tokens.color.grey_30_light};
+    border: 1px solid ${theme === 'light' ? tokens.color.grey_30_light : tokens.color.grey_30_dark};
     color: ${tokens.color.grey_5_light};
     font-weight: 500;
     font-size: 16px;
@@ -92,6 +102,10 @@ export function Button({ theme, text, link, size, type }: Props) {
         'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
         'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
       src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css');
+    }
+
+    &:hover {
+      background-color: ${tokens.color.main_green_20};
     }
   `;
 
