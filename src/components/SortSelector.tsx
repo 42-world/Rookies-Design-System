e8ab3770c8f653e8fd02bb9466aa79238e53617e
GulSam00 <gulsamcono@gmail.com>
2022-11-12 18:10:01 +0900

    Fix : 프론트에서 css 변경 확인

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -21,14 +21,16 @@ export const SortSelector = ({ list, theme, focus }: Props) => {
     <div className={containerStyle}>
       {list.map((item, index) => {
         return (
-          <div
-            className={css`
-              color: ${onFocus === index ? tokens.color.main_green_10 : tokens.color.grey_40_light};
-              font-size: 100px;
-            `}
-            onClick={() => setOnFocus(index)}
-          >
-            {item.text}
+          <div className="item">
+            <text
+              className={css`
+                color: ${onFocus === index ? tokens.color.main_green_10 : tokens.color.grey_40_light};
+                font-size: 100px;
+              `}
+              onClick={() => setOnFocus(index)}
+            >
+              {item.text}
+            </text>
           </div>
         );
       })}
@@ -37,13 +39,18 @@ export const SortSelector = ({ list, theme, focus }: Props) => {
 };
 
 const containerStyle = css`
+
+
 height: "100%",
 display: "flex",
-flexDirection: "column",
-justifyContent: "start",
+flex-direction: column,
 gap: "20px",
-alignItems: "start",
-overflow: "hidden",
+al: "start",
 color: "red",
+font-size: "24px",
 cursor: "pointer",
+
+.item::before {
+  content: "•";
+}
 `;
