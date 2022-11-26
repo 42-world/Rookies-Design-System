821d212a58402b7993e1ffe8b025cdd3ca04ae8c
GulSam00 <gulsamcono@gmail.com>
2022-11-26 23:41:52 +0900

    Fix : css 로직 함수로 뺌

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -26,20 +26,8 @@ export const SortSelector = ({ list, theme, focus }: Props) => {
       {list.map((item, index) => {
         return (
           <div className="item">
-            <text
-              className={css`
-                color: ${onFocus === index ? tokens.color.main_green_10 : tokens.color.grey_40_light};
-                margin: 4px;
-              `}
-            >
-              •
-            </text>
-            <text
-              className={css`
-                color: ${onFocus === index ? tokens.color.grey_40_light : tokens.color.grey_40_dark};
-              `}
-              onClick={() => setOnFocus(index)}
-            >
+            <text className={beforeItem(onFocus === index)}>•</text>
+            <text className={itemText(onFocus === index)} onClick={() => setOnFocus(index)}>
               {item.text}
             </text>
           </div>
@@ -52,4 +40,13 @@ export const SortSelector = ({ list, theme, focus }: Props) => {
 const containerStyle = css`
   display: flex;
   flex-direction: row;
+`;
+
+const beforeItem = (isFocus: boolean) => css`
+  color: ${isFocus ? tokens.color.main_green_10 : tokens.color.grey_40_light};
+  margin: 4px;
+`;
+
+const itemText = (isFocus: boolean) => css`
+  color: ${isFocus ? tokens.color.grey_40_light : tokens.color.grey_40_dark};
 `;
