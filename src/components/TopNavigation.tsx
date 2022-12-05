d9645f149feca579038d4a49bf6ed04359fb3393
Son Beomsoo <k1a11220@naver.com>
2022-12-05 11:06:35 +0900

    feat: Icon 컴포넌트 분리

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/css';
 import * as React from 'react';
+import Icons from '../assets/icons';
 import { tokens } from '../tokens';
 import { Text } from '../typography/Text';
 
@@ -77,17 +78,9 @@ export function TopNavigation({ theme, links, user }: Props) {
         <div className={iconListStyle}>
           <a href={links.themeLink} className={iconContainerStyle}>
             {theme === 'light' ? (
-              <svg viewBox="0 0 20 20" fill="currentColor" width="24" height="24" color={tokens.color.grey_50_light}>
-                <path
-                  fillRule="evenodd"
-                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <Icons.Sun color={tokens.color.grey_50_light} />
             ) : (
-              <svg viewBox="0 0 20 20" fill="currentColor" width="24" height="24" color={tokens.color.grey_50_dark}>
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-              </svg>
+              <Icons.Moon color={tokens.color.grey_50_dark} />
             )}
 
             <Text
@@ -99,47 +92,15 @@ export function TopNavigation({ theme, links, user }: Props) {
             />
           </a>
           <a href={links.searchLink} className={iconContainerStyle}>
-            <svg
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              width="24"
-              height="24"
-              color={theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark}
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <Icons.Search color={theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark} />
             <Text align="center" color="grey_50" size="Caption" text="검색" theme={theme} />
           </a>
           <a href={links.createLink} className={iconContainerStyle}>
-            <svg
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              width="24"
-              height="24"
-              color={theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark}
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <Icons.Plus color={theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark} />
             <Text align="center" color="grey_50" size="Caption" text="글쓰기" theme={theme} />
           </a>
           <a href={links.alertLink} className={iconContainerStyle}>
-            <svg
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              width="24"
-              height="24"
-              color={theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark}
-            >
-              <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 16a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
-            </svg>
+            <Icons.Bell color={theme === 'light' ? tokens.color.grey_50_light : tokens.color.grey_50_dark} />
             <Text align="center" color="grey_50" size="Caption" text="알림" theme={theme} />
           </a>
           <a href={user.profileLink} className={iconContainerStyle}>
