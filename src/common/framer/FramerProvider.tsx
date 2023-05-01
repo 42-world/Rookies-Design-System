d5135cff79a1a4e2fb4522c0b3a338c70ac3ff38
Seojunhwan <seojunhwan@kakao.com>
2023-05-01 18:16:05 +0900

    feat: framer reset css

--- src/common/framer/FramerProvider.tsx
+++ src/common/framer/FramerProvider.tsx
@@ -1,6 +1,11 @@
+import { resetCss } from '../../assets/styles/resetCss';
 import { ThemeProvider } from '../../context';
 import { useFramerThemeData } from './useFramerThemeData';
 
 export function FramerProvider({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider value={useFramerThemeData() ? 'dark' : 'light'}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider value={useFramerThemeData() ? 'dark' : 'light'}>
+      <div className={resetCss}>{children}</div>
+    </ThemeProvider>
+  );
 }
