601ab0bf2bb52750d337b2ed9e0505981188c2c2
chichoon <chichoon.choi@gmail.com>
2023-05-28 17:52:29 +0900

    style: selected 되지 않았을 때 배경색 스타일 추가

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -15,15 +15,15 @@ export function Chip({ children, text, selected }: Props) {
       className={cx({
         'w-fit h-8 flex flex-row items-center border-2 rounded-lg border-amber-400 select-none': true,
         [text ? 'px-3' : 'px-2']: true,
-        'bg-bg-secondary': selected,
-        'bg-bg-secondary_dark': selected && theme === 'dark',
+        [selected ? 'bg-bg-secondary' : 'bg-bg-tertiary_alpha_0']: theme === 'light',
+        [selected ? 'bg-bg-secondary_dark' : 'bg-bg-primary_alpha_0_dark']: theme === 'dark',
       })}
     >
       <div className={cx([{ hidden: !children }, 'mr-2'])}>{children}</div>
       <span
         className={cx({
-          [selected ? 'text-text-primary_dark' : 'text-text-secondary_dark']: theme === 'dark',
           [selected ? 'text-text-primary' : 'text-text-secondary']: theme === 'light',
+          [selected ? 'text-text-primary_dark' : 'text-text-secondary_dark']: theme === 'dark',
           'text-sm font-medium': true,
         })}
       >
