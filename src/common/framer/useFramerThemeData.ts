4c7d44314dd9bb858cfae8c592e0829799fb20bb
huni <petercha2000@gmail.com>
2023-06-04 03:17:52 +0900

    fix: framer theme

--- src/common/framer/useFramerThemeData.ts
+++ src/common/framer/useFramerThemeData.ts
@@ -7,19 +7,29 @@ export function useFramerThemeData() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    const handleChange = (e: WindowEventMap['message']) => {
-      // framer 에서 테마를 변경할때마다 이벤트가 발생한다.
+    // Listen for OS theme changes
+    const handleOsThemeChange = (e: MediaQueryListEvent) => {
+      setIsDarkMode(e.matches);
+    };
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    darkModeQuery.addEventListener('change', handleOsThemeChange);
+
+    // Set initial theme
+    setIsDarkMode(darkModeQuery.matches);
+
+    // Listen for theme changes from Framer
+    const handleFramerThemeChange = (e: WindowEventMap['message']) => {
       if ('appearance' in e.data) {
         setIsDarkMode(e.data.appearance === 'dark');
         return;
       }
     };
-
-    window.addEventListener('message', handleChange);
+    window.addEventListener('message', handleFramerThemeChange);
 
     // Cleanup listener on unmount
     return () => {
-      window.removeEventListener('message', handleChange);
+      window.removeEventListener('message', handleFramerThemeChange);
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', handleOsThemeChange);
     };
   }, []);
 
