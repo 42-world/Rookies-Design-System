e67ab6a657e712180285fb81ce92b140c9710c0f
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-06-04 00:36:12 +0900

    feat: ChevronRight 아이콘 추가

---
+++ src/assets/icons/ChevronRight.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  classname?: string;
+}
+
+export const ChevronRight = ({ classname }: Props) => {
+  return (
+    <svg className={`${classname}`} viewBox="0 0 24 24" fill="currentColor">
+      <path d="M9.29 15.46l3.88-3.88L9.29 7.7a.9959.9959 0 0 1 0-1.41c.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z"></path>
+    </svg>
+  );
+};
