404df6cb03d3d25b3c2011c3b1b6bf15a0eb5512
huni <petercha2000@gmail.com>
2023-06-04 03:16:45 +0900

    fix: reset css 제거 및 스크립트 수정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
 import { ThemeProvider } from '../src';
 import '../src/styles/_tailwind.css';
-import '../src/styles/reset.css';
 import './style.css';
 
 const preview: Preview = {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import './styles/tailwind.css';
+import './styles/_tailwind.css';
 
 export * from './components';
 export * from './context';
