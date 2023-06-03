00000d4ca2b5faa766b36ae5bf9fecf623a1cfbd
chichoon <chichoon.choi@gmail.com>
2023-06-04 00:55:23 +0900

    fix: 이미지 주소 변경

--- src/components/Card/Card.stories.ts
+++ src/components/Card/Card.stories.ts
@@ -10,7 +10,7 @@ const meta: Meta<Component> = {
 type Story = StoryObj<Component>;
 export const Primary: Story = {
   args: {
-    imageSrc: 'https://github.com/42-world/42world-Frontend/raw/dev/public/assets/42worldCharacterLogo.png',
+    imageSrc: 'https://picsum.photos/536/354',
     imageAlt: '42world',
     primary: '에베베손',
     secondary: '2023.04.32',
@@ -19,7 +19,7 @@ export const Primary: Story = {
 
 export const PrimaryWithDetail: Story = {
   args: {
-    imageSrc: 'https://github.com/42-world/42world-Frontend/raw/dev/public/assets/42worldCharacterLogo.png',
+    imageSrc: 'https://picsum.photos/536/354',
     imageAlt: '42world',
     primary: '에베베손',
     secondary: '2023.04.32',
@@ -29,7 +29,7 @@ export const PrimaryWithDetail: Story = {
 
 export const PrimaryNoPrimaryText: Story = {
   args: {
-    imageSrc: 'https://github.com/42-world/42world-Frontend/raw/dev/public/assets/42worldCharacterLogo.png',
+    imageSrc: 'https://picsum.photos/536/354',
     imageAlt: '42world',
     secondary: '지최',
     detail: '지최는 배가 고프고 내일은 닭가슴살 소세지 먹을 거고 스타벅스 가서 이거 하고',
