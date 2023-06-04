9af6515988b46c67ff17d0366b3ca24e9da246e7
Seojunhwan <seojunhwan@kakao.com>
2023-06-05 00:25:58 +0900

    fix: change Input dark mode context

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,6 @@
 import cx from 'classnames';
 import type { InputHTMLAttributes, ReactNode } from 'react';
 import { forwardRef, useId } from 'react';
-import { useTheme } from '../../context';
 import { useControllableState } from './useControllableState';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
@@ -33,7 +32,6 @@ export const Input = forwardRef<HTMLInputElement, Props>(
     ref,
   ) => {
     const id = useId();
-    const theme = useTheme();
     const [value, setValue] = useControllableState({
       state: valueFromProps,
       defaultState: defaultValue ?? '',
@@ -50,21 +48,13 @@ export const Input = forwardRef<HTMLInputElement, Props>(
           {label && (
             <label
               htmlFor={idFromProps ?? id}
-              className={cx('before:content-["*"] before:mr-1 text-sm leading-[1.8]  font-normal', {
-                'text-text-secondary': theme === 'light',
-                'text-text-secondary_dark': theme === 'dark',
-              })}
+              className='before:content-["*"] before:mr-1 text-sm leading-[1.8] font-normal text-text-secondary dark:text-text-secondary_dark'
             >
               {label}
             </label>
           )}
           {maxLength && (
-            <span
-              className={cx('text-sm leading-[1.8] font-normal ml-auto', {
-                'text-text-secondary': theme === 'light',
-                'text-text-secondary_dark': theme === 'dark',
-              })}
-            >
+            <span className="text-sm leading-[1.8] font-normal ml-auto text-text-secondary dark:text-text-secondary_dark">
               ({value.length}/{maxLength})
             </span>
           )}
@@ -73,17 +63,14 @@ export const Input = forwardRef<HTMLInputElement, Props>(
           className={cx('flex items-center w-full rounded-lg px-4', {
             'border border-solid border-border-primary bg-bg-primary_alpha_0 focus-within:border-color-blue_200':
               variant === 'outline',
-            [theme === 'light' ? 'bg-bg-secondary' : 'bg-bg-secondary_dark']: variant === 'filled',
+            'bg-bg-secondary dark:bg-bg-secondary_dark': variant === 'filled',
             'border-color-red focus-within:border-color-red': hasError,
           })}
         >
           <input
             ref={ref}
             id={idFromProps ?? id}
-            className={cx('w-full h-[44px] bg-transparent focus:outline-none text-base font-normal leading-[1.5]', {
-              'text-text-primary placeholder:text-text-tertiary': theme === 'light',
-              'text-text-primary_dark placeholder:text-text-tertiary_dark': theme === 'dark',
-            })}
+            className="w-full h-[44px] bg-transparent focus:outline-none text-base font-normal leading-[1.5] text-text-primary dark:text-text-primary_dark placeholder:text-text-tertiary dark:placeholder:text-text-tertiary_dark"
             value={value}
             onChange={handleChange}
             maxLength={maxLength}
@@ -93,9 +80,7 @@ export const Input = forwardRef<HTMLInputElement, Props>(
         </div>
         {subLabel && (
           <span
-            className={cx('text-sm leading-[1.8] font-normal', {
-              'text-text-secondary': theme === 'light',
-              'text-text-secondary_dark': theme === 'dark',
+            className={cx('text-sm leading-[1.8] font-normal text-text-secondary dark:text-text-secondary_dark', {
               '!text-color-red': hasError,
             })}
           >
