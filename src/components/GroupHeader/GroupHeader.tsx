f4e4b1f4e04cef6916c3ca80d761e8259ed7b760
Seojunhwan <seojunhwan@kakao.com>
2023-06-05 00:21:02 +0900

    fix: change GroupHeader dark mode context

--- src/components/GroupHeader/GroupHeader.tsx
+++ src/components/GroupHeader/GroupHeader.tsx
@@ -1,6 +1,4 @@
-import cx from 'classnames';
 import { ChevronRight } from '../../assets/icons';
-import { useTheme } from '../../context';
 import { Text } from '../Text';
 
 interface Props {
@@ -9,18 +7,13 @@ interface Props {
 }
 
 export function GroupHeader({ href, text }: Props) {
-  const theme = useTheme();
-
   return (
     <a
-      className={cx('flex flex-row items-center gap-1 hover:gap-2 transition-gap duration-300 ease-in-out', {
-        'text-text-primary': theme === 'light',
-        'text-text-primary_dark': theme === 'dark',
-      })}
+      className="flex flex-row items-center gap-1 hover:gap-2 transition-gap duration-300 ease-in-out text-text-primary dark:text-text-primary_dark"
       href={href}
     >
       <Text text={text} size="heading3" weight="semibold" />
-      <ChevronRight classname={cx('w-6', 'h-6')} />
+      <ChevronRight className="w-6 h-6" />
     </a>
   );
 }
