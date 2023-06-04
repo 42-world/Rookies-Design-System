09ccc7e3019b0788979218b895c8243761e9f166
Seojunhwan <seojunhwan@kakao.com>
2023-06-04 10:08:03 +0900

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,6 @@ module.exports = {
          * fontFamily는 tailwind.css 에 별도로 정의되어 있으므로 여기에 추가하지 마세요
          */
       },
-      bg: {
-        primary: '#FFFFFF',
-        primary_dark: '#0A0A0A',
-        primary_alpha_0: '#FFFFFF00',
-        primary_alpha_0_dark: '#00000000',
-        secondary: '#F4F5F6',
-        secondary_dark: '#292929',
-        tertiary: '#E3E3E3',
-        tertiary_dark: '#333333',
-        tertiary_alpha_0: '#E3E3E300',
-        tertiary_alpha_0_dark: '#33333300',
-        alpha_60: '#FFFFFF99',
-        alpha_60_dark: '#00000099',
-      },
       aspectRatio: {
         '16/10': '16 / 10',
       },
@@ -36,7 +22,7 @@ module.exports = {
           primary: '#000000',
           primary_dark: '#FFFFFF',
           secondary: '#707070',
-          secondary_dark: '#BABABA',
+          secondary_dark: '#292929',
           tertiary: '#B5B5B5',
           tertiary_dark: '#7A7A7A',
         },
