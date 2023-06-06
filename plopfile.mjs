007b85f5a25832211311772f3af52d4bdc1dbed6
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-06-06 20:32:20 +0900

    feat: plopfile generator 설정
    - prompt로 component name을 입력.
    - index, component, story, framer 파일을 생성

---
+++ plopfile.mjs
@@ -0,0 +1,37 @@
+export default function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop,
+) {
+  plop.setGenerator('component', {
+    description: 'Create a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/index.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/component.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.ts',
+        templateFile: 'templates/story.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.framer.tsx',
+        templateFile: 'templates/framer.hbs',
+      },
+    ],
+  });
+}
