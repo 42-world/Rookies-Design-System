afc150f1f064cba5fe1a7082280fabcfe1c42fb4
chichoon <chichoon.choi@gmail.com>
2023-06-10 23:51:15 +0900

    feat: :sparkles: 아바타 텍스트 추가

---
+++ src/components/AvatarText/AvatarText.tsx
@@ -0,0 +1,22 @@
+import cx from 'classnames';
+import { Avatar, Text } from '..';
+
+interface Props {
+  src: string;
+  alt?: string;
+  size?: '40px' | '64px';
+  mainText: string;
+  subText?: string;
+}
+
+export function AvatarText({ src, alt, size = '40px', mainText, subText }: Props) {
+  return (
+    <div className={cx('flex flex-row', { 'gap-3': size === '40px', 'gap-4': size === '64px' })}>
+      <Avatar src={src} alt={alt} size={size} />
+      <div className="flex flex-col items-start justify-center">
+        <Text text={mainText} size={size === '64px' ? 'heading3' : 'body2'} weight="medium" />
+        {subText && <Text text={subText} size={size === '64px' ? 'body2' : 'small'} color="secondary" />}
+      </div>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
+export { Avatar } from './Avatar';
+export { AvatarText } from './AvatarText';
 export { Card } from './Card';
 export { Chip } from './Chip';
+export { Divider } from './Divider';
 export { GroupHeader } from './GroupHeader';
+export { Input } from './Input';
 export { Sample } from './Sample';
 export { Text } from './Text';
 export { Thumbnail } from './Thumbnail';
-export { Divider } from './Divider';
-export { Input } from './Input';
