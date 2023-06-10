63e05dc9be3b5fb26f6ff057977d22f04309914c
huni <petercha2000@gmail.com>
2023-06-11 05:04:15 +0900

    fix: framer theme

--- src/common/framer/useFramerThemeData.ts
+++ src/common/framer/useFramerThemeData.ts
@@ -12,19 +12,25 @@ export function useFramerThemeData() {
       setIsDarkMode(e.matches);
     };
     const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const isOsDarkMode = darkModeQuery.matches;
     darkModeQuery.addEventListener('change', handleOsThemeChange);
-
-    // Set initial theme
-    setIsDarkMode(darkModeQuery.matches);
 
     // Listen for theme changes from Framer
     const handleFramerThemeChange = (e: WindowEventMap['message']) => {
       if ('appearance' in e.data) {
         setIsDarkMode(e.data.appearance === 'dark');
-        return;
       }
     };
     window.addEventListener('message', handleFramerThemeChange);
+    const isFramerDarkMode = window.document.querySelector('body')?.classList.contains('framer-theme-dark') ?? false;
+    const isFramerEditor = window.location.href.includes('framercanvas');
+
+    // Set the initial theme
+    if (isFramerEditor) {
+      setIsDarkMode(isFramerDarkMode);
+    } else {
+      setIsDarkMode(isOsDarkMode);
+    }
 
     // Cleanup listener on unmount
     return () => {
