41a410b00c01873bcba83fdbc8f0ed97727942b4
chichoon <chichoon.choi@gmail.com>
2023-06-11 13:47:01 +0900

    fix: type 선언 수정 (구동이 도와줌)

--- src/components/IconText/IconText.tsx
+++ src/components/IconText/IconText.tsx
@@ -1,25 +1,27 @@
 import cx from 'classnames';
 import { FunctionComponent, SVGProps } from 'react';
 import { Icon, Text } from '..';
+import { TextSize } from '../Text';
 
-type TextSize = 'heading1' | 'heading2' | 'heading3' | 'body1' | 'body2' | 'small';
+type Size = 'medium' | 'small' | 'xsmall';
+
 interface Props {
   svg: FunctionComponent<SVGProps<SVGSVGElement>>;
   text: string;
-  size?: 'medium' | 'small' | 'xsmall';
+  size?: Size;
 }
 
 export function IconText({ svg, size = 'medium', text }: Props) {
-  const textSize = {
+  const textSize: Record<Size, TextSize> = {
     medium: 'body1',
     small: 'body2',
     xsmall: 'small',
-  }[size];
+  };
 
   return (
     <div className={cx('flex flex-row items-center', { 'gap-2': size !== 'xsmall', 'gap-1': size === 'xsmall' })}>
       <Icon svg={svg} size={size} className="fill-text-secondary dark:fill-text-secondary_dark" />
-      <Text text={text} size={textSize as TextSize} color="secondary" />
+      <Text text={text} size={textSize[size]} color="secondary" />
     </div>
   );
 }
