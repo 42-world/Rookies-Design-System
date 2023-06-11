17b827ab80b478b4749610269184b51fb7c13218
huni <petercha2000@gmail.com>
2023-06-12 03:01:12 +0900

    fix: custom svg build

---
+++ .build/plugin.svg.js
@@ -0,0 +1,41 @@
+const { readFileSync } = require('fs');
+
+const { transform } = require('@svgr/core');
+
+exports.svgPlugin = (options = {}) => ({
+  name: 'svgr',
+  setup(build) {
+    build.onResolve({ filter: /\.svg$/ }, async (args) => {
+      switch (args.kind) {
+        case 'import-statement':
+        case 'require-call':
+        case 'dynamic-import':
+        case 'require-resolve':
+          return;
+        default:
+          return {
+            external: true,
+          };
+      }
+    });
+
+    build.onLoad({ filter: /\.svg$/ }, async (args) => {
+      const svg = readFileSync(args.path, { encoding: 'utf8' });
+      const contents = await transform(svg, { ...options }, { filePath: args.path });
+
+      if (args.suffix === '?url') {
+        return {
+          contents: args.path,
+          loader: 'text',
+        };
+      }
+
+      return {
+        contents: `export default () => {
+          return (${contents});
+        }`,
+        loader: options.typescript ? 'tsx' : 'jsx',
+      };
+    });
+  },
+});
