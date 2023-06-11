58a67ad6d13c517b0ee27535ffdfac2b4549bdd7
huni <petercha2000@gmail.com>
2023-06-11 11:47:59 +0900

    chore: declare

--- declarations.d.ts
+++ declarations.d.ts
@@ -6,6 +6,3 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
-// NOTE: The declaration below was injected by `"framer"`
-// see https://www.framer.com/docs/guides/handshake for more information.
-declare module "https://framer.com/m/*";
