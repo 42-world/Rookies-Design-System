568279206ad2aba82efb848a086078f10a65aa1c
huni <petercha2000@gmail.com>
2023-06-15 23:41:50 +0900

    fix: svg component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,4 +1,4 @@
-import { createElement, SVGProps } from 'react';
+import { SVGProps } from 'react';
 
 interface Props extends SVGProps<SVGSVGElement> {
   svg: React.FC<React.SVGAttributes<SVGElement>>;
@@ -14,17 +14,13 @@ export function Icon({ svg: SvgComponent, size = 'medium', className, ...rest }:
     xlarge: { width: 32, height: 32 },
   }[size] || { width: 24, height: 24 };
 
-  const origin = SvgComponent({});
-  return createElement(
-    'svg',
-    {
-      ...origin?.props,
-      viewBox: '0 0 24 24',
-      width: wh.width,
-      height: wh.height,
-      className: `fill-black dark:fill-white ${className}`,
-      ...rest,
-    },
-    origin?.props.children,
+  return (
+    <SvgComponent
+      viewBox="0 0 24 24"
+      width={wh.width}
+      height={wh.height}
+      className={`fill-black dark:fill-white ${className}`}
+      {...rest}
+    />
   );
 }
