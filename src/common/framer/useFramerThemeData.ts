834011233667b5948286c3da9ec68a5011ba7e76
huni <petercha2000@gmail.com>
2023-06-16 01:54:01 +0900

    fix: is Framer editor

--- src/common/framer/useFramerThemeData.ts
+++ src/common/framer/useFramerThemeData.ts
@@ -7,6 +7,25 @@ export function useFramerThemeData() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
+    const isFramerEditor = window.location.href.includes('framercanvas');
+    if (isFramerEditor) {
+      // Listen for theme changes from Framer
+      const handleFramerThemeChange = (e: WindowEventMap['message']) => {
+        if ('appearance' in e.data) {
+          setIsDarkMode(e.data.appearance === 'dark');
+        }
+      };
+      window.addEventListener('message', handleFramerThemeChange);
+      const isFramerDarkMode = window.document.querySelector('body')?.classList.contains('framer-theme-dark') ?? false;
+
+      // Set the initial theme
+      setIsDarkMode(isFramerDarkMode);
+
+      return () => {
+        window.removeEventListener('message', handleFramerThemeChange);
+      };
+    }
+
     // Listen for OS theme changes
     const handleOsThemeChange = (e: MediaQueryListEvent) => {
       setIsDarkMode(e.matches);
@@ -15,26 +34,11 @@ export function useFramerThemeData() {
     const isOsDarkMode = darkModeQuery.matches;
     darkModeQuery.addEventListener('change', handleOsThemeChange);
 
-    // Listen for theme changes from Framer
-    const handleFramerThemeChange = (e: WindowEventMap['message']) => {
-      if ('appearance' in e.data) {
-        setIsDarkMode(e.data.appearance === 'dark');
-      }
-    };
-    window.addEventListener('message', handleFramerThemeChange);
-    const isFramerDarkMode = window.document.querySelector('body')?.classList.contains('framer-theme-dark') ?? false;
-    const isFramerEditor = window.location.href.includes('framercanvas');
-
     // Set the initial theme
-    if (isFramerEditor) {
-      setIsDarkMode(isFramerDarkMode);
-    } else {
-      setIsDarkMode(isOsDarkMode);
-    }
+    setIsDarkMode(isOsDarkMode);
 
     // Cleanup listener on unmount
     return () => {
-      window.removeEventListener('message', handleFramerThemeChange);
       window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', handleOsThemeChange);
     };
   }, []);
