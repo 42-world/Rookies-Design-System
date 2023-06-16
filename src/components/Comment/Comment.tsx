34eff6e9cd2e04a53a8412104d0f7ea0a264f32f
chichoon <chichoon.choi@gmail.com>
2023-06-16 16:19:50 +0900

    feat: comment 좋아요 누르면 색상변경

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import { AvatarText, Badge, Button, Text } from '..';
 
 interface Props {
@@ -34,7 +35,11 @@ export function Comment({
       </div>
       <Text size="body2" text={content} />
       <div className="flex flex-row items-center gap-4">
-        <Button text={`좋아요 ${numOfLikes}`} variant="text" className="px-0" />
+        <Button
+          text={`좋아요 ${numOfLikes}`}
+          variant="text"
+          className={cx('px-0', { '[&>p]:text-color-system_200 [&>p]:dark:text-color-system_200': isLiked })}
+        />
         <Button text="답글달기" variant="text" className="px-0 mx-4" />
         <Button text="수정하기" variant="text" className="px-0" />
       </div>
