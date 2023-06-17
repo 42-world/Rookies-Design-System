03f53cbe3c99e1a10bf7c5e892ca9aa2a9a766fc
chichoon <chichoon.choi@gmail.com>
2023-06-18 01:35:45 +0900

    fix: footer button link로 변경

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,17 +5,21 @@ import Mail from '@material-design-icons/svg/filled/mail.svg';
 import Github from '../../assets/icons/Github.svg';
 
 interface Props {
-  onClickGithub?: () => void;
-  onClickMail?: () => void;
+  githubLink?: string;
+  mailLink?: string;
   text: string;
 }
 
-export function Footer({ onClickGithub, onClickMail, text }: Props) {
+export function Footer({ githubLink = '', mailLink = '', text }: Props) {
   return (
     <div className="flex flex-col">
       <div className="mb-6 w-full flex flex-row align-center justify-center gap-2">
-        <Button variant="secondary" onClick={onClickGithub} icon={Github} />
-        <Button variant="secondary" onClick={onClickMail} icon={Mail} />
+        <a href={githubLink} target="_blank" className="w-fit h-fit inline-block">
+          <Button variant="secondary" icon={Github} />
+        </a>
+        <a href={mailLink} target="_blank" className="w-fit h-fit inline-block">
+          <Button variant="secondary" icon={Mail} />
+        </a>
       </div>
       <div className="flex align-center justify-center">
         <Text size="body2" color="secondary" text={text} />
