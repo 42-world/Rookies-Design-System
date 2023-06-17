36f0e8688637f97b70bea41858206baffa03b6d0
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-06-18 00:58:19 +0900

    feat: ListItem 메타데이터 외부 주입 가능하도록 compound 패턴 적용

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,63 +1,55 @@
-import ChatBubble from '@material-design-icons/svg/filled/chat_bubble.svg';
-import Favorite from '@material-design-icons/svg/filled/favorite.svg';
+import { PropsWithChildren } from 'react';
 import { Divider } from '../Divider';
-import { Icon } from '../Icon';
 import { Thumbnail } from '../Thumbnail';
 import { Text } from './../Text/Text';
 
-interface Props {
+type Props = PropsWithChildren<{
   title: string;
   thumbnailSrc?: string;
   secondaryTextFirst?: string;
   secondaryTextSecond?: string;
-  commentCnt?: number;
-  likeCnt?: number;
   hasBorder?: boolean;
-}
+}>;
 
 const MergedSecondaryText = (first?: string, second?: string) => {
   return (
     <div className="flex flex-row items-center gap-1">
-      {first && <Text text={first} size="body2" weight="medium" color="secondary" />}
-      {first && second && <Text text="·" size="body2" weight="medium" color="secondary" />}
+      {first && <Text text={first} size="body2" weight="regular" color="secondary" />}
+      {first && second && <Text text="·" size="body2" weight="regular" color="secondary" />}
       {second && <Text text={second} size="body2" weight="regular" color="secondary" />}
     </div>
   );
 };
 
 export function ListItem({
+  children,
   title,
   thumbnailSrc,
   secondaryTextFirst,
   secondaryTextSecond,
-  commentCnt,
-  likeCnt,
   hasBorder = true,
 }: Props) {
   return (
-    <div className="w-[540px] cursor-pointer active:scale-95 transition-transform duration-300 ease-out select-none">
-      <div className="flex flex-row items-center py-4">
-        {thumbnailSrc && <Thumbnail src={thumbnailSrc} alt={title} width={85} ratio="16:9" />}
-        <div className="ml-3 flex-1">
-          {MergedSecondaryText(secondaryTextFirst, secondaryTextSecond)}
-          <Text text={title} size="body1" weight="medium" color="primary" />
+    <div className="w-full cursor-pointer active:scale-95 transition-transform duration-300 ease-out select-none">
+      <div className="flex flex-row justify-between items-center py-4">
+        <div className="flex flex-row items-center">
+          {thumbnailSrc && <Thumbnail src={thumbnailSrc} alt={title} width={85} ratio="16:9" />}
+          <div className="ml-3 flex-1">
+            {MergedSecondaryText(secondaryTextFirst, secondaryTextSecond)}
+            <Text text={title} size="body1" weight="medium" color="primary" />
+          </div>
         </div>
-        <div className="flex flex-row items-center gap-2 text-text-secondary dark:text-text-secondary_dark">
-          {commentCnt && (
-            <div className="flex flex-row items-center gap-2">
-              <Icon svg={ChatBubble} className="w-4 h-4" />
-              <Text text={commentCnt.toString()} size="body2" weight="regular" color="secondary" />
-            </div>
-          )}
-          {likeCnt && (
-            <div className="flex flex-row items-center gap-2">
-              <Icon svg={Favorite} className="w-4 h-4" />
-              <Text text={likeCnt.toString()} size="body2" weight="regular" color="secondary" />
-            </div>
-          )}
-        </div>
+        {children}
       </div>
       {hasBorder && <Divider variant="secondary" />}
     </div>
   );
 }
+
+function MetadataItems({ children }: Pick<Props, 'children'>) {
+  return (
+    <div className="flex flex-row items-center gap-2 text-text-secondary dark:text-text-secondary_dark">{children}</div>
+  );
+}
+
+ListItem.MetadataItems = MetadataItems;
