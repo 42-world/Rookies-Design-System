798ba1b9c466a2edeefcd42513776a61f965756c
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-06-22 01:49:31 +0900

    feat: Searchbar forwardRef 적용

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import Search from '@material-design-icons/svg/filled/search.svg';
 import { Button } from '../Button';
 import { twMerge } from 'tailwind-merge';
@@ -17,19 +17,22 @@ const variantStyles: Record<SearchBarVariant, string> = {
     'bg-border-primary dark:bg-border-primary_dark focus-within:bg-color-system_200 dark:focus-within:bg-color-system_200 hover:shadow-searchbar_solid_hover dark:hover:shadow-searchbar_solid_hover_dark',
 };
 
-export function SearchBar({ variant = 'primary', className, ...restProps }: Props) {
-  return (
-    <div className={twMerge('relative h-10 w-full overflow-hidden rounded-[20px] p-0.5', variantStyles[variant])}>
-      <div className="flex h-full w-full flex-row items-center rounded-[20px] bg-bg-primary pl-4 pr-1 dark:bg-bg-primary_dark">
-        <input
-          className={twMerge(
-            'h-full flex-1 border-0 bg-inherit text-sm font-medium leading-6 outline-0 placeholder:text-text-tertiary dark:text-text-tertiary_dark dark:placeholder:text-text-tertiary_dark',
-            className,
-          )}
-          {...restProps}
-        />
-        <Button type="submit" variant="text" icon={Search} aria-label="검색" />
+export const SearchBar = forwardRef<HTMLInputElement, Props>(
+  ({ variant = 'primary', className, ...restProps }, ref) => {
+    return (
+      <div className={twMerge('relative h-10 w-full overflow-hidden rounded-[20px] p-0.5', variantStyles[variant])}>
+        <div className="flex h-full w-full flex-row items-center rounded-[20px] bg-bg-primary pl-4 pr-1 dark:bg-bg-primary_dark">
+          <input
+            ref={ref}
+            className={twMerge(
+              'h-full flex-1 border-0 bg-inherit text-sm font-medium leading-6 outline-0 placeholder:text-text-tertiary dark:text-text-tertiary_dark dark:placeholder:text-text-tertiary_dark',
+              className,
+            )}
+            {...restProps}
+          />
+          <Button type="submit" variant="text" icon={Search} aria-label="검색" />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  },
+);
