3340af73ae42d715f14ec8331fba77fe80157d0f
chichoon <chichoon.choi@gmail.com>
2023-06-23 17:06:53 +0900

    fix: feed framer 추가

--- src/components/Feed/Feed.framer.tsx
+++ src/components/Feed/Feed.framer.tsx
@@ -12,4 +12,55 @@ export function Feed(props: ComponentProps<typeof _Feed>) {
 }
 
 // Add your custom property infos here
-// applyFramerProperties(Feed, {});
+applyFramerProperties(Feed, {
+  username: {
+    title: 'Username',
+    type: ControlType.String,
+    defaultValue: '에베베손',
+  },
+  timestamp: {
+    title: 'Timestamp',
+    type: ControlType.String,
+    defaultValue: '1시간 전',
+  },
+  userProfileSrc: {
+    title: 'UserProfileSrc',
+    type: ControlType.String,
+    defaultValue: 'https://picsum.photos/536/354',
+  },
+  thumbnailSrc: {
+    title: 'ThumbnailSrc',
+    type: ControlType.String,
+    defaultValue: 'https://picsum.photos/536/354',
+  },
+  title: {
+    title: 'Title',
+    type: ControlType.String,
+    defaultValue: '42world design system',
+  },
+  contents: {
+    title: 'Contents',
+    type: ControlType.String,
+    defaultValue: '42world design system',
+  },
+  isLiked: {
+    title: 'IsLiked',
+    type: ControlType.Boolean,
+    defaultValue: false,
+  },
+  likeCount: {
+    title: 'LikeCount',
+    type: ControlType.Number,
+    defaultValue: 0,
+  },
+  isCommentClicked: {
+    title: 'IsCommentClicked',
+    type: ControlType.Boolean,
+    defaultValue: false,
+  },
+  commentCount: {
+    title: 'CommentCount',
+    type: ControlType.Number,
+    defaultValue: 0,
+  },
+});
