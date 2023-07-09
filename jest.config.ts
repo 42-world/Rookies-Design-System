295a6ef77ea19de5693f00069cb2a8e484d25ef5
Seojunhwan <seojunhwan@kakao.com>
2023-07-09 10:49:24 +0900

    chore: ci script 추가

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,14 @@ const config: Config = {
   verbose: true,
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  collectCoverageFrom: [
+    'src/{components,utils}/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.*',
+    '!src/**/index.ts',
+    '!src/**/index.tsx',
+    '!src/**/*.framer.tsx',
+  ],
 };
 
 export default config;
